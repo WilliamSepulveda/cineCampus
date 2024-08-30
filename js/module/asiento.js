45a5788a700fc99fb65f3d4448c4c8130cd562b4
WilliamSepulveda <160752969+WilliamSepulveda@users.noreply.github.com>
2024-08-30 14:20:12 -0500

    docs :memo: I updated the README file and documented the functions.

--- js/module/asiento.js
+++ js/module/asiento.js
@@ -16,7 +16,22 @@ module.exports = class Asientos extends connect {
         this.collectionFunciones = 'funcion';
         this.collectionLugares = 'lugar';
     }
-
+    /**
+     * Consulta la disponibilidad de asientos en una sala para una proyección específica.
+     * 
+     * Este método realiza lo siguiente:
+     * 1. Verifica si la proyección (función) existe en la base de datos.
+     * 2. Obtiene todos los asientos disponibles en la sala asociada a la proyección.
+     * 3. Obtiene todas las boletas asociadas a la proyección para identificar los asientos ocupados.
+     * 4. Marca cada asiento como "Disponible" o "No Disponible" en función de si está asociado a una boleta.
+     * 
+     * @param {string} idFuncion - El identificador único de la proyección para la cual se desea consultar la disponibilidad de asientos. Debe ser un string representando un ObjectId en MongoDB.
+     * @param {ObjectId} idLugar - El identificador único del lugar donde se encuentra la sala. Debe ser un ObjectId que representa el lugar en la base de datos.
+     * @returns {Promise<Object>} Una promesa que se resuelve con un objeto que contiene:
+     *   - `funcion`: Información de la proyección, incluyendo `id_pelicula`, `id_lugar`, `fecha_hora_inicio`, y `fecha_hora_fin`.
+     *   - `asientos`: Una lista de asientos disponibles en la sala para la proyección dada, con cada asiento marcado como "Disponible" o "No Disponible".
+     * @throws {Error} Lanza un error si ocurre un problema durante la consulta o si la proyección no se encuentra.
+    */
     async consultarDisponibilidad(idFuncion, idLugar) {
         try {
             await this.open(); 
