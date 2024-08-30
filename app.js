cea4c6020271b24a565c5869b0d40f4b478c89a7
WilliamSepulveda <160752969+WilliamSepulveda@users.noreply.github.com>
2024-08-30 10:41:17 -0500

    feat :sparkles: ticket purchase
    I created a function that allows the purchase of tickets for a specific movie, including the selection of the date and time of the screening.

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const Pelicula = require ("./js/module/pelicula");
 const { ObjectId } = require('mongodb')
+const Boletas = require('./js/module/boleta');
 
 /**
  * ejemplo de uso para listar todas las peliculas
@@ -14,11 +15,30 @@ const { ObjectId } = require('mongodb')
 //     });
 
 
-let obj = new Pelicula();
-obj.getSpecificMovies("66d0777d3170ffb8c89f4bb4")
-    .then(res=>{
-        console.log(res);
+// let obj = new Pelicula();
+// obj.getSpecificMovies("66d0777d3170ffb8c89f4bb4")
+//     .then(res=>{
+//         console.log(res);
+//     })
+//     .catch(err=>{
+//         console.log(err);
+//     })
+
+
+const idBoleta = "66d07cde3170ffb8c89f4bd9"; 
+const tipoMovimiento = {
+    id: 2,
+    nombre: "compra"
+};
+
+const newBoletas = new Boletas();
+
+newBoletas.BuyBoletas(idBoleta, tipoMovimiento)
+    .then(res => {
+        console.log("Operación completada.");
+        console.log("Detalles de la boleta:", res.boleta);
+        console.log("Tipo de movimiento:", res.movimiento);
     })
-    .catch(err=>{
-        console.log(err);
-    })
+    .catch(err => {
+        console.error("Error en la operación:", err);
+    });
