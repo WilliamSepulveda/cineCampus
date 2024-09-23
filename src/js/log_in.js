1689828adcaa8c0744c3898b6348edc7ca839a69
WilliamSepulveda <160752969+WilliamSepulveda@users.noreply.github.com>
2024-09-23 08:23:38 -0500

    release :bookmark: creacion del diseño del cineCampulands
    feat :lipstick: se importan las imagenes para las plantillas de login, create, y el index
    Hash: f6041f23
    
    feat :lipstick: se crea el diseño del create, login, e index
    Hash: 6ca55606
    creando las plantillas y los estilos
    
    feat :sparkles: se valida el correo y la validación del login
    Hash: d777877aa
    creando archivo js de login que permite validar el correo y la contraseña del login
    
    feat :lipstick: se importaron las imagenes del homeCineCampus y cree el diseño
    Hash: 55e29f26
    haciendolo responsive
    
    feat :lipstick: se añade funcion de dot en la plantilla de movies
    Hash: ab8b3ed
    usando javascript para permitir la fucnioalidad de los botones
    
    feat :lipstick: se crea la seleccion de pelicula
    Hash: 01685794
    creando la plantilla con el diseño e importando las imagenes para su diseño
    
    feat :lipstick: se creo el diseño de choose seat
    Hash: ca3724d2
    finalizando la plantilla con el diseño e importando una imagen
    
    chore :lipstick: se crea redireccion de los botones para la siguente pagina
    Hash: 0c4b878
    usando a href
    
    feat :lipstick: se crea el order_summary con los diseños
    Hash: c49a97b1
    importandos las imagenes necesarias y haciendo que sea responsivo
    
    feat :lipstick: se crea el diseño de los tickets
    Hash: 2c8a0011
    chore :lipstick: se crea el boton para redirigir a la plantilla de tickets
    Hash: 26852215
    
    feat :lipstick: se añaden funcionalidades a los modulos de choose y cinema_select
    Hash: 04ecb570
    creando los javascript y permitiendo mayor realismo a una pagina de compra de boletos
    
    chore :lipstick: se mejora el dieño del login home y create account
    Hash: 36c5e879

---
+++ src/js/log_in.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const loginForm = document.getElementById('login_form');
+  
+    if (loginForm) {
+      loginForm.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        const email = document.getElementById('email').value;
+        const password = document.getElementById('password').value;
+  
+        if (!/^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/.test(email)) {
+          alert('Por favor ingrese un correo electrónico válido.');
+          return;
+        }
+  
+        try {
+          const url = `${e.target.action}?email=${encodeURIComponent(email)}&password=${encodeURIComponent(password)}`;
+          
+          // Realizar la solicitud GET
+          const response = await fetch(url, {
+            method: 'GET',
+            headers: { 'Content-Type': 'application/json' },
+          });
+  
+          const result = await response.json();
+  
+          if (response.ok) {
+            alert(result.message); 
+            window.location.href = result.redirectUrl; 
+          } else {
+            alert(result.message); 
+          }
+        } catch (error) {
+          console.error('Error durante el inicio de sesión:', error);
+          alert(`Ocurrió un error durante el inicio de sesión: ${error.message}`);
+        }
+      });
+    }
+  });
+  
